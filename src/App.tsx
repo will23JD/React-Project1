6ccbfd429243618e0b38366c5c8938b15af29444
Will Darling <Will_JD@outlook.com>
2024-05-03 16:08:27 +0100

    Connected to firebase for google login and auth, simple css style

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react'; 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import { Main } from './pages/main';
-import { Login } from './pages/login';
-import { Navbar } from './components/navbar';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Main } from "./pages/main";
+import { Login } from "./pages/login";
+import { Navbar } from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Main/>}/>
-          <Route path='/login' element={<Login/>}/>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
