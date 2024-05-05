9d2df1640f2b51d0dd0e255ef17ec7f23ffa6664
Will Darling <Will_JD@outlook.com>
2024-05-05 12:55:51 +0100

    setup creating post with firebase, yup and react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Main } from "./pages/main";
 import { Login } from "./pages/login";
+import { CreatePost } from "./pages/create-post/create-post";
 import { Navbar } from "./components/navbar";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/createpost" element={<CreatePost />} />
         </Routes>
       </Router>
     </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,7 +14,11 @@ export const Navbar = () => {
     <div className="navbar">
       <div className="links">
         <Link to="/"> Home </Link>
-        <Link to="/login"> Login </Link>
+        {!user ? (
+          <Link to="/login"> Login </Link>
+        ) : (
+          <Link to="/createpost"> Creat Post </Link>
+        )}
       </div>
       <div className="user">
         {user && (
